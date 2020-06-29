f59a7d66a433d6e8c9cc9929e185e56419a756e8
adiii717 <adil.mehmood@zigron.com>
2020-06-29 05:48:39 +0500

    add vuejs example application

---
+++ app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{mess}} </h1>
+    <ul>
+      <li v-for="(p,index) in products" :key="index" v-text="p">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld 
+  },
+  data (){
+    return {
+      mess : process.env.VUE_APP_TITLE,
+      products : []
+    }
+  },
+  created (){
+this.products= this.loadprod();
+  },
+  methods: {
+ loadprod() {
+   return [
+     "prod1",
+     "prod2",
+               "prod1"
+   ]
+ }
+  }
+    
+  
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
